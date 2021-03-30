063db7751875e4e1c842fb49a45beb9a915a10a6
koleh <koleh.david@balancecarbon.com>
2021-03-30 18:42:23 +1030

    adding files for lecture 3

---
+++ javascripts/page.js
@@ -0,0 +1,146 @@
+var posts = [
+    {
+        title: "An Example Post",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur maximus, lorem non varius consequat, ipsum magna vestibulum lectus, et fringilla tellus augue id nisl. Donec tempus est a hendrerit ornare.",
+        tags: ["tag1","tag2","tag3"],
+        date: new Date("1 JAN 1901"),
+        upvotes: 9001
+    },
+    {
+        title: "Another Example Post",
+        content: "Proin convallis, urna nec consequat mattis, erat mauris volutpat neque, vitae facilisis enim est eu felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla in odio id lorem facilisis dapibus quis ac mi. Cras semper ipsum eu sem sodales dapibus.",
+        tags: ["tag1","tag4"],
+        date: new Date("2 JAN 1902"),
+        upvotes: 42
+    }
+];
+var search = null;
+
+/*
+ * Hides the main part of the page to show the Ask a Question section
+ */
+function showAsk(){
+    var main = document.getElementById("main");
+    var ask = document.getElementById("ask");
+    main.style.display = "none";
+    ask.style.display = "block";
+}
+
+/*
+ * Hides the Ask a Question section of the page to show the main part,
+ * clearing the question input fields.
+ */
+function showMain(){
+    var main = document.getElementById("main");
+    var ask = document.getElementById("ask");
+    ask.style.display = "none";
+    main.style.display = "block";
+
+    document.getElementById('post-title').value = '';
+    document.getElementById('post-content').value = '';
+    document.getElementById('post-tags').value = '';
+}
+
+/*
+ * Creates a new question/post & appends it to the global array of posts, before triggering an update and the main part of the page.
+ */
+function createPost(){
+
+    search = null;
+
+    let post = {
+        title: document.getElementById('post-title').value,
+        content: document.getElementById('post-content').value,
+        tags: document.getElementById('post-tags').value.split(" "),
+        date: new Date(),
+        upvotes: 0
+    };
+
+    posts.push(post);
+
+    updatePosts();
+    showMain();
+
+}
+
+/*
+ * Updates the search term then reloads the posts shown
+ */
+function searchPosts(){
+
+    search = document.getElementById('post-search').value.toUpperCase();
+    updatePosts();
+
+}
+
+
+/*
+ * Reloads the posts shown on the page
+ * Iterates over the array of post objects, rendering HTML for each and appending it to the page
+ * If a search term is being used
+ */
+function updatePosts() {
+
+    // Reset the page
+    document.getElementById('post-list').innerHTML = '';
+
+    // Iterate over each post in the array by index
+    for(let i=0; i<posts.length; i++){
+
+        let post = posts[i];
+
+        // Check if a search term used.
+        if(search !== null){
+            // If so, skip this question/post if title or content doesn't match
+            if (post.title.toUpperCase().indexOf(search) < 0 &&
+                post.content.toUpperCase().indexOf(search) < 0 ) {
+                continue;
+            }
+        }
+
+        // Generate a set of spans for each of the tags
+        let tagSpans = '';
+        for(let tag of post.tags){
+            tagSpans = tagSpans + `<span class="tag">${tag}</span>`;
+        }
+
+        // Generate the post/question element and populate its inner HTML
+        let postDiv = document.createElement("DIV");
+        postDiv.classList.add("post");
+
+        postDiv.innerHTML = `
+            <div class="votes">
+                <button onclick="upvote(${i})">+</button>
+                <p><span class="count">${post.upvotes}</span><br />votes</p>
+                <button onclick="downvote(${i})">-</button>
+            </div>
+            <div class="content">
+                <h3><a href="#">${post.title}</a></h3>
+                <p>${post.content}</p>
+                ${tagSpans}<span class="date">${post.date.toLocaleString()}</span>
+            </div>
+        `;
+
+        // Append the question/post to the page
+        document.getElementById("post-list").appendChild(postDiv);
+
+    }
+
+
+}
+
+/*
+ * Increase the votes for a given post, then update the page
+ */
+function upvote(index) {
+    posts[index].upvotes ++;
+    updatePosts();
+}
+
+/*
+ * Decrease the votes for a given post, then update the page
+ */
+function downvote(index) {
+    posts[index].upvotes --;
+    updatePosts();
+}
